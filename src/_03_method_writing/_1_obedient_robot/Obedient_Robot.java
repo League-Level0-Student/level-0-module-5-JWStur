79af4778ae60ce27ec9790f11251e8aa2658b427
root <root@2d5974757fe6>
2020-10-03 21:25:03 +0000

    working on happy pet

--- src/_03_method_writing/_1_obedient_robot/Obedient_Robot.java
+++ src/_03_method_writing/_1_obedient_robot/Obedient_Robot.java
@@ -1,19 +1,85 @@
 package _03_method_writing._1_obedient_robot;
 
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Obedient_Robot {
+	
+static Robot robot = new Robot();
+
+	
 public static void main(String[] args) {
-	jump();
-	int total = add(6,7);
-	System.out.println(total);
+	
+		
+    String[] options2 = {"Red", "Green", "Blue"};
+    int input2 = JOptionPane.showOptionDialog(null, "Returns the position of your choice on the array",
+            "Click a button",
+            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options2, options2[0]);
+    if (input2 == 0) {
+    	robot.setPenColor(255,0,0);
+    }
+    if (input2 == 1) {
+    	robot.setPenColor(0,255,0);
+    }
+    if (input2 == 2) {
+    	robot.setPenColor(0,0,255);
+    }
+	
+    String[] options = {"Square", "Triangle", "Circle"};
+    int input = JOptionPane.showOptionDialog(null, "Returns the position of your choice on the array",
+            "Click a button",
+            JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+    if (input == 0) {
+    	drawSquare();
+    }
+    if (input == 1) {
+    	drawTriangle();
+    }
+    if (input == 2) {
+    	drawCircle();
+    }
+
 }
 
-public  static void drawSquare() {
-	System.out.println("Jumping");
+public static void drawSquare() {
+	robot.penDown();
+	//robot.setPenColor(0,0,255);
+	robot.setSpeed(100);
+	robot.hide();
+	for (int i = 1; i <= 4; i++) {	
+		robot.move(90);
+		robot.turn(90);
+	}
+}
+
+public static void drawTriangle() {
+	robot.penDown();
+	//robot.setPenColor(0,0,255);
+	robot.setSpeed(100);
+	robot.hide();
+	for (int i = 1; i <= 3; i++) {
+		robot.move(60);
+		robot.turn(120);
+
+		}
+	}
+
+
+public static void drawCircle() {
+	robot.penDown();
+	//robot.setPenColor(0,0,255);
+	robot.setSpeed(100);
+	robot.hide();
+	for (int i = 1; i <= 360; i++) {
+		robot.move(1);
+		robot.turn(1);
+		}
+	}
 }
 
 
-public static int add(int n1, int n2) {
-	int total = n1 + n2;
-	return total;
-}
-}
+
+
+
+
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -14,16 +14,44 @@ public class HappyPet {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do for your pet?", "HappyPet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "cuddle", "food", "water", "take a walk", "groom", "clean up poop" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
 
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+			if(task == 0) {
+			
+			}
+	
+//respond()
+			
 	}
-
+	public static void cuddle(){
+		
+	}
+	
+	public static void food() {
+		
+	}
+	
+	public static void water() {
+		
+	}
+	
+	public static void takeWalk() {
+		
+	}
+	
+	public static void groom() {
+		
+	}
+	
+	public static void cleanPoop() {
+		
+	}
+	
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
